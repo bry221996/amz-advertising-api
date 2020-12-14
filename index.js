de930101ca6732b7054376b3a9e49a51fdb387ca
Bryan Mulingbayan <bmulingbayan@yondu.com>
2020-12-14 23:23:41 +0800

    add client

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * Module dependencies.
+ */
+var AdvertisingClient = require("./AdvertisingClient");
+
+/**
+ * Expose `Strategy` directly from package.
+ */
+exports = module.exports = AdvertisingClient;
+
+/**
+ * Expose constructors.
+ */
+exports.Class = AdvertisingClient;
